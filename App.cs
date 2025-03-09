bf1be9020bc22ee8604df8ac4bdb0558e7f13afa
Полина Старичкова <polinastar02@yandex.ru>
2025-03-09 17:06:00 +0700

    .

--- App.cs
+++ App.cs
@@ -11,24 +11,6 @@ namespace MyPlugin
     {
         public Result OnStartup(UIControlledApplication application)
         {
-            //string tabName = "Мои плагины";
-            //try { application.CreateRibbonTab(tabName); } catch { }
-
-            //RibbonPanel panel = application.CreateRibbonPanel(tabName, "Инструменты");
-
-            //string assemblyPath = Assembly.GetExecutingAssembly().Location;
-
-            //PushButtonData buttonData = new PushButtonData("AI for Revit", "AI for Revit", assemblyPath, "MyPlugin.MyCommand");
-
-            //PushButton pushButton = panel.AddItem(buttonData) as PushButton;
-
-            //Uri smallIcon = new Uri(@"C:\Users\IRINA\Desktop\Папка Полины С\диплом\MyPlugin\Resources\icon.png");
-            //Uri largeIcon = new Uri(@"C:\Users\IRINA\Desktop\Папка Полины С\диплом\MyPlugin\Resources\icon.png");
-
-            //pushButton.Image = new BitmapImage(smallIcon);
-            //pushButton.LargeImage = new BitmapImage(largeIcon);
-
-
             string assemblyLocation = Assembly.GetExecutingAssembly().Location,
                     iconsDirectoryPath = Path.GetDirectoryName(assemblyLocation) + @"\icons\";
             string tabName = "Мои плагины";
